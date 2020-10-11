f66fec29542c9d0acbc340c99bd3191f8b574c1f
osmangonigithub <osmangone939@gmail.com>
2020-10-11 12:53:25 +0600

    cors problem

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const serviceAccount = require(__dirname+"/serviceaccount.json");
 
 
 const app = express();
-app.use(cors());
+// app.use(cors())
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
