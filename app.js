c73c56d4aacd111420e2663fe4d087c7b35c4aa5
osmangonigithub <osmangone939@gmail.com>
2020-10-11 10:53:54 +0600

    done

--- app.js
+++ app.js
@@ -1,18 +1,26 @@
-const express=require('express');
+const express = require('express');
 const cors=require('cors');
-const bodyParser=require('body-parser');
+const bodyParser = require('body-parser');
 const MongoClient = require('mongodb').MongoClient;
 const objectId = require('mongodb').ObjectId;
 require('dotenv').config();
+const admin = require("firebase-admin");
+// const serviceAccount = require(__dirname+"/serviceaccount.json");
 
-const app=express();
-app.use(cors())
+
+const app = express();
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/', (req,res)=>{
+// admin.initializeApp({
+//   credential: admin.credential.cert(serviceAccount),
+//   databaseURL: "https://volunteer-network-e5d98.firebaseio.com"
+// });
+app.get('/',(req, res)=> {
   res.send('Hello World')
 })
+
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.koqo3.mongodb.net/volunteer-network?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true});
 client.connect(err => {
@@ -27,9 +35,18 @@ client.connect(err => {
   })
 
   app.get('/all-registered-events',(req,res)=>{
+    // const token=req.headers.authorization;
+
+    // if(token && token.startsWith('Bearer')){
+    //   const idToken=token.split(' ')[1];
+     
+    //   admin.auth().verifyIdToken(idToken)
+    //   .then(function(decodedToken) {
+    //     let uid = decodedToken.uid;
         allRegistriedTasksCollection.find({})
         .toArray((error,documents)=>{
           res.send(documents)
+          
         })
       .catch(function(error) {
         
@@ -38,10 +55,22 @@ client.connect(err => {
   })
 
   app.get('/my-events',(req,res)=>{
+    // const token=req.headers.authorization;
+
+    // if(token && token.startsWith('Bearer')){
+    //   const idToken=token.split(' ')[1];
+     
+    //   admin.auth().verifyIdToken(idToken)
+    //   .then(function(decodedToken) {
+    //     let uid = decodedToken.uid;
+
         allRegistriedTasksCollection.find({email:req.query.email})
         .toArray((error,documents)=>{
           res.send(documents)
         })
+      .catch(function(error) {
+        
+      });
     // }
   
   })
@@ -76,5 +105,4 @@ client.connect(err => {
   
 });
 
-
-app.listen(process.env.PORT || 5000)
+app.listen(process.env.PORT ||5000)
